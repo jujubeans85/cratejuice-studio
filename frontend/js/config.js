e466e2f75ea308a79cbabb5c7beca44fd5dbec2b
CJN2D2 <juice@plurconsulting.com>
2025-12-12 10:28:28 +1100

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,4 +1,4 @@
 export const QR_API_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:8000/api/qr/create"
-    : "https://cratejuice.onrender.com/api/qr/create";
+    : "https://cratejuice-press-backend.onrender.com/api/qr/create"
